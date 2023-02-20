7261f633a7381606b5cef29cb1242771dbace5c4
Victor <virb3e@gmail.com>
2023-02-20 01:49:59 +0000

    fix: rework script, add deinterlace and detelecine

--- README.md
+++ README.md
@@ -4,14 +4,23 @@ A workflow for upscaling and interpolating videos with NVIDIA TensorRT accelerat
 
 ## Setup
 
-1. Download and extract all dependencies:
+1. Download and extract all external dependencies:
 
-   | Source                                                                                                                              | Files                         | Destination                      |
-   | ----------------------------------------------------------------------------------------------------------------------------------- | ----------------------------- | -------------------------------- |
-   | [vsmlrt-windows-x64-cuda.v13.7z](https://github.com/AmusementClub/vs-mlrt/releases/download/v13/vsmlrt-windows-x64-cuda.v13.7z)     | \*                            | ./runtime/vsmlrt/                |
-   | [python-3.11.1-embed-amd64.zip](https://www.python.org/ftp/python/3.11.1/python-3.11.1-embed-amd64.zip)                             | \*                            | ./runtime/                       |
-   | [vapoursynth-classic-R57.A6-x64.zip](https://github.com/AmusementClub/vapoursynth-classic/releases/download/R57.A6/release-x64.zip) | \*                            | ./runtime/                       |
-   | [ffms2-2.40-msvc.7z](https://github.com/FFMS/ffms2/releases/download/2.40/ffms2-2.40-msvc.7z)                                       | ffms2-2.40-msvc/x64/ffms2.dll | ./runtime/vapoursynth64/plugins/ |
+   | Source                                                       | Files             | Destination                      |
+   | ------------------------------------------------------------ | ----------------- | -------------------------------- |
+   | [vsmlrt-windows-x64-cuda.v13.7z](https://github.com/AmusementClub/vs-mlrt/releases/download/v13/vsmlrt-windows-x64-cuda.v13.7z) | \*                | .\runtime\vsmlrt\                |
+   | [python-3.10.10-embed-amd64.zip](https://www.python.org/ftp/python/3.10.10/python-3.10.10-embed-amd64.zip) | \*                | .\runtime\                       |
+   | [VapourSynth64-Portable-R61.7z](https://github.com/vapoursynth/vapoursynth/releases/download/R61/VapourSynth64-Portable-R61.7z) | \*                | .\runtime\                       |
+   | [vsutil-0.8.0.zip](https://github.com/Irrational-Encoding-Wizardry/vsutil/archive/refs/tags/0.8.0.zip) | .\vsutil\         | .\runtime\                       |
+   | [vivtc-r1.7z](https://github.com/vapoursynth/vivtc/releases/download/R1/vivtc-r1.7z) | .\win64\VIVTC.dll | .\runtime\vapoursynth64\plugins\ |
+
+1. Download and install plugins:
+
+   ```bash
+   cd runtime
+   .\python.exe .\vsrepo.py -p update
+   .\python.exe .\vsrepo.py -p install ffms2 havsfunc akarin
+   ```
 
 1. Start upscaling:
 
--- inference.py
+++ inference.py
@@ -1,7 +1,7 @@
 import sys
 import os
 from pathlib import Path
-from math import ceil
+import functools
 
 import vapoursynth as vs
 from vapoursynth import core
@@ -14,15 +14,23 @@ core.std.LoadPlugin(path="vstrt.dll")
 os.chdir(script_dir)
 
 from runtime.vsmlrt.vsmlrt import CUGAN, RIFE, Backend
+from runtime.havsfunc import QTGMC
 
-# settings
+# =============================================================================
+# SETTINGS
+# =============================================================================
+
 core.num_threads = 2
-target_width: int = 1920
-target_height: int = 1080
-pad: bool = False
-target_scale: int = 2
+# prescale to closest fit within these dimensions, preserving aspect ratio
+prescale_w = 960  # None = disable
+prescale_h = 540  # None = disable
 scale_func = core.resize.Lanczos
-interpolate_factor: int = 2
+
+colorspace = None  # auto
+# colorspace = [4, 1, 4]  # NTSC (old)
+# colorspace = [5, 1, 5]  # PAL
+# colorspace = [6, 1, 6]  # NTSC
+
 backend = Backend.TRT(
     fp16=True,
     output_format=1,  # fp16
@@ -35,6 +43,52 @@ backend = Backend.TRT(
     use_cuda_graph=True,
 )
 
+deinterlace_mode = None  # True = top field first, False = bottom field first
+deinterlace_func = lambda clip: QTGMC(clip, Preset="Slow", FPSDivisor=1)
+
+detelecine_mode = None  # True = top field first, False = bottom field first
+
+
+def detelecine_func(clip):
+    def postprocess(n, f, clip, deinterlaced):
+        if f.props["_Combed"] > 0:
+            return deinterlaced
+        else:
+            return clip
+
+    matched_clip = vs.core.vivtc.VFM(clip, 1 if detelecine_mode else 0)
+    deinterlaced_clip = QTGMC(matched_clip, TFF=detelecine_mode, FPSDivisor=2)
+    clip = vs.core.std.FrameEval(
+        matched_clip,
+        functools.partial(
+            postprocess, clip=matched_clip, deinterlaced=deinterlaced_clip
+        ),
+        prop_src=matched_clip,
+    )
+    clip = vs.core.vivtc.VDecimate(clip)
+    return clip
+
+
+upscale_factor: int = 2  # 1 = disable
+upscale_func = lambda clip: CUGAN(
+    clip,
+    version=2,
+    noise=-1,
+    scale=upscale_factor,
+    backend=backend,
+)
+
+interpolate_factor: int = 1  # 1 = disable
+interpolate_func = lambda clip: RIFE(
+    clip,
+    multi=interpolate_factor,
+    scale=1.0,
+    model=46,
+    backend=backend,
+)
+
+# =============================================================================
+
 
 def eprint(*args, **kwargs):
     """
@@ -43,123 +97,105 @@ def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 
-def rescale(clip, target_width: int, target_height: int, pad: bool):
-    """
-    Rescale to target dimensions, optionally padding with black pixels where necessary.
-    Transforms non-square pixels to square (1:1 SAR).
-    """
-    # transform non-square pixels to square
-    sar = [props.get(p, 1) for p in ["_SARNum", "_SARDen"]]
-    fixed_width = clip.width * max(1, sar[0] / sar[1])
-    fixed_height = clip.height * max(1, sar[1] / sar[0])
-    # find common factor that will match one dimension and fit the other inside target
-    size_factor_w = target_width / fixed_width
-    size_factor_h = target_height / fixed_height
-    if size_factor_w < size_factor_h:
-        size_factor = size_factor_w
-        matched_width = True
-    else:
-        size_factor = size_factor_h
-        matched_width = False
-    # resize to match first dimension
-    clip = scale_func(
-        clip,
-        fixed_width * size_factor,
-        fixed_height * size_factor,
-    )
-    if pad:
-        # pad to match second dimension
-        border_l = 0
-        border_r = 0
-        border_t = 0
-        border_b = 0
-        if matched_width:
-            # https://github.com/vapoursynth/vapoursynth/blob/2ee76bc5163d546e6a296142a6664a29fe7df165/src/core/simplefilters.cpp#L311
-            divisor = 1 << clip.format.subsampling_h
-            border_count = (target_height - clip.height) // divisor
-            border_t = (border_count // 2) * divisor
-            border_b = ceil(border_count / 2) * divisor
-        else:
-            divisor = 1 << clip.format.subsampling_w
-            border_count = (target_width - clip.width) // divisor
-            border_l = (border_count // 2) * divisor
-            border_r = ceil(border_count / 2) * divisor
-        clip = core.std.AddBorders(clip, border_l, border_r, border_t, border_b)
-    return clip
-
-
-# load clip
 clip = core.ffms2.Source(
     source=dict(globals())["input"],
     cache=False,
 )
+
+if deinterlace_mode is not None:
+    # do this before getting props so the change is registered
+    clip = core.std.SetFieldBased(clip, 2 if deinterlace_mode else 1)
+
 props = clip.get_frame(0).props
 
-if props.get("_FieldBased", 0) != 0:
-    raise Exception("Video is interlaced, deinterlace it first.")
+if detelecine_mode is not None:
+    eprint("Detelecining video")
+    clip = scale_func(clip, format=vs.YUV420P8)
+    clip = detelecine_func(clip)
+elif deinterlace_mode is not None:
+    eprint("Deinterlacing video")
+    clip = deinterlace_func(clip)
+elif props.get("_FieldBased", 0) != 0:
+    raise Exception("video is interlaced, but deinterlace_mode is disabled, aborting")
 
-# rescale to produce target dimensions after upscale
-clip = rescale(clip, target_width // target_scale, target_height // target_scale, pad)
+# transform non-square pixels to square
+sar = [props.get(p, 1) for p in ["_SARNum", "_SARDen"]]
+square_w = clip.width * max(1, sar[0] / sar[1])
+square_h = clip.height * max(1, sar[1] / sar[0])
+
+# find common factor that will match one dimension and fit the other inside prescale dimensions
+# make sure both dimensions are divisible by their subsampling factor
+size_factor_w = prescale_w / square_w if prescale_w is not None else sys.maxsize
+size_factor_h = prescale_h / square_h if prescale_h is not None else sys.maxsize
+# https://github.com/vapoursynth/vapoursynth/blob/2ee76bc5163d546e6a296142a6664a29fe7df165/src/core/simplefilters.cpp#L311
+divisor_w = 1 << clip.format.subsampling_w
+divisor_h = 1 << clip.format.subsampling_h
+if size_factor_w < size_factor_h:
+    temp_w = (square_w * size_factor_w + divisor_w - 1) // divisor_w * divisor_w
+    temp_h = (square_h * size_factor_w + divisor_h - 1) // divisor_h * divisor_h
+else:
+    temp_w = (square_w * size_factor_h + divisor_w - 1) // divisor_w * divisor_w
+    temp_h = (square_h * size_factor_h + divisor_h - 1) // divisor_h * divisor_h
+
+clip = scale_func(clip, temp_w, temp_h)
+
+target_w = temp_w * upscale_factor
+target_h = temp_h * upscale_factor
+
+if upscale_factor > 1:
+    # CUGAN requires multiples of 2
+    temp_w = (temp_w + 1) // 2 * 2
+    temp_h = (temp_h + 1) // 2 * 2
 
 if interpolate_factor > 1:
     # skip interpolating on scene change
     clip = core.misc.SCDetect(clip=clip, threshold=0.100)
 
 # default to 1 (BT.709) if prop is None or 2 (undefined)
-colorspace = [
-    x if x != 2 else 1
-    for x in [props.get(p, 2) for p in ["_Matrix", "_Transfer", "_Primaries"]]
-]
+if colorspace is None:
+    colorspace = [
+        x if x != 2 else 1
+        for x in [props.get(p, 2) for p in ["_Matrix", "_Transfer", "_Primaries"]]
+    ]
 
-# models only accept RGB
+# models only accept RGB, also pad to expected dimensions
 clip = scale_func(
     clip,
+    temp_w,
+    temp_h,
     format=vs.RGBS,
     matrix_in=colorspace[0],
     transfer_in=colorspace[1],
     primaries_in=colorspace[2],
+    src_width=temp_w,
+    src_height=temp_h,
 )
 
 # color can exceed 1.0 with CUGAN, causing graphical glitches
 clip = core.std.Limiter(clip, max=1.0, planes=[0, 1, 2])
+# clip = core.akarin.Expr(clip, "x 0 1 clamp")
 
-clip = CUGAN(
-    clip,
-    version=2,
-    noise=-1,
-    scale=2,
-    backend=backend,
-)
-
-# if using model over target_scale, downscale
-if clip.width % target_width != 0 or clip.height % target_height != 0:
-    raise Exception(f"bad clip dimensions")
-clip = scale_func(clip, target_width, target_height)
+if upscale_factor > 1:
+    clip = upscale_func(clip)
 
 if interpolate_factor > 1:
     # RIFE requires multiples of 32
-    th = (clip.height + 31) // 32 * 32
-    tw = (clip.width + 31) // 32 * 32
-    clip = scale_func(clip, tw, th, src_width=tw, src_height=th)
-    clip = RIFE(
-        clip,
-        multi=interpolate_factor,
-        scale=1.0,
-        model=46,
-        backend=backend,
-    )
+    temp_w = (target_w + 31) // 32 * 32
+    temp_h = (target_h + 31) // 32 * 32
+    clip = scale_func(clip, temp_w, temp_h, src_width=temp_w, src_height=temp_h)
+    clip = interpolate_func(clip)
 
 # convert to YUV420 (BT.709) for h264 encoding
 clip = scale_func(
     clip,
-    target_width,
-    target_height,
+    target_w,
+    target_h,
     format=vs.YUV420P8,
     matrix=1,
     transfer=1,
     primaries=1,
-    src_width=target_width,
-    src_height=target_height,
+    src_width=target_w,
+    src_height=target_h,
 )
 
 clip.set_output()
